8b019ceb3dbdd2383f57c26979c7730bbd84c482
ItziarIriarte <itziangeles@gmail.com>
2019-12-20 04:24:25 -0400

    Proteción

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,14 @@ import {LoginPageComponent} from "./componentes/login-page/login-page.component"
 import {RegisterPageComponent} from "./componentes/register-page/register-page.component";
 import {AsignacionPageComponent} from "./componentes/asignacion-page/asignacion-page.component";
 import {NotFoundPageComponent} from "./componentes/not-found-page/not-found-page.component";
+
+import {AuthGuard} from './guards/auth.guard';
+
 const routes: Routes = [
   { path: '', component: HomePageComponent},
   { path:'login', component:LoginPageComponent},
   {path: 'register', component:RegisterPageComponent},
-  {path:'asignacion', component:AsignacionPageComponent},
+  {path:'asignacion', component:AsignacionPageComponent, canActivate:[AuthGuard]},
   {path:'**', component: NotFoundPageComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {AuthService} from './servicios/auth.service';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {environment} from '../environments/environment';
+import {AuthGuard} from './guards/auth.guard';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,7 @@ import {environment} from '../environments/environment';
     AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
