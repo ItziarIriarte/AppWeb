e66886ed0d307c55b41d70a0274df07493029bad
JoseRivera <josephrivera248@gmail.com>
2019-12-19 22:18:42 -0400

    Conexión Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyAoxcITvT3N1XNBqBHtWHMkkq1dsaiRIKo",
+    authDomain: "asignacion-91654.firebaseapp.com",
+    databaseURL: "https://asignacion-91654.firebaseio.com",
+    projectId: "asignacion-91654",
+    storageBucket: "asignacion-91654.appspot.com",
+    messagingSenderId: "857301830373",
+    appId: "1:857301830373:web:0be592331e0736cae9220c",
+    measurementId: "G-0YL8HEQ4QP"
+  }
 };
